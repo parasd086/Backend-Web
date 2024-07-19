23699d5f39f5b1b333659d793f00be4b464b2c26
parasd086 <parasd086@gmail.com>
2024-07-19 12:59:57 +0530

    Testing: Jest assertions and matchers

--- Testing TS and Nodejs/src/test/Utils.test.ts
+++ Testing TS and Nodejs/src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
   //test naming is expressive.
@@ -13,5 +13,37 @@ describe("Utils test suite", () => {
 
     //assert:
     expect(actual).toBe(expected); //we are expecting our actual value is our expected
+  });
+
+  it.only("should return info for valid string", () => {
+    const actual = getStringInfo("My-String");
+
+    expect(actual.lowerCase).toBe("my-string");
+    expect(actual.extraInfo).toEqual({});
+
+    expect(actual.characters.length).toBe(9);
+    expect(actual.characters).toHaveLength(9);
+
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ]);
+    expect(actual.characters).toContain<string>("M"); //if it contains 'M' element which is in string fromat.
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"])
+    ); //check for arrays when we dont know the order of the element
+
+    //checking for undefined
+    expect(actual.extraInfo).not.toBe(undefined);
+    expect(actual.extraInfo).not.toBeUndefined();
+    expect(actual.extraInfo).toBeDefined();
+    expect(actual.extraInfo).toBeTruthy();
   });
 });
